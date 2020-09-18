38b6e5eb1b0470edd7e01b2f2b3870e6e3235b13
Steven630 <64087266@163.com>
2020-09-18 11:09:16 +0800

    try

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
 git clone https://github.com/Steven630/KindleEar.git
 python helper.py
-appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
+appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml --noauth_local_webserver
 appcfg.py update KindleEar/
